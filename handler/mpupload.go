a7e7cb4a812aefb5d7a4ea6a14dfaf7d65a23239
cyf <cyfjava@163.com>
2019-08-07 18:23:27 +0800

    分块上传

--- handler/mpupload.go
+++ handler/mpupload.go
@@ -6,6 +6,7 @@ import (
 	"go-cloud/util"
 	"math"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -37,5 +38,37 @@ func InitialMultipartUploadHandler(w http.ResponseWriter, r *http.Request) {
 		ChunkSize:  5 * 1024 * 1024,
 		ChunkCount: int(math.Ceil(float64(filesize) / (5 * 1024 * 1024))),
 	}
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "chunkcount", upInfo.ChunkCount)
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "filehash", upInfo.FileHash)
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "filesize", upInfo.FileSize)
 
+	w.Write(util.NewRespMsg(0, "OK", upInfo).JSONBytes())
+}
+
+func UploaadPartHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	//username:=r.Form.Get("username")
+	uploadID := r.Form.Get("uploadid")
+	chunkIndex := r.Form.Get("index")
+
+	rConn := redis.RedisPool().Get()
+	defer rConn.Close()
+	fd, err := os.Create("/Users/cyf/Pictures/data/" + uploadID + "/" + chunkIndex)
+	if err != nil {
+		w.Write(util.NewRespMsg(-1, "Upload part failed", nil).JSONBytes())
+		return
+	}
+	defer fd.Close()
+	buf := make([]byte, 1024*1024)
+	for {
+		n, err := r.Body.Read(buf)
+		fd.Write(buf[:n])
+		if err != nil {
+			break
+		}
+	}
+
+	rConn.Do("HSET", "MP_"+uploadID, "chkidx_"+chunkIndex, 1)
+
+	w.Write(util.NewRespMsg(0, "OK", nil).JSONBytes())
 }
